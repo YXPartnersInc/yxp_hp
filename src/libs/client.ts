c80d7f49941c3c5e8098eb41b3265854ade318b3
TakenakaEri <takenakaeri0423@gmail.com>
2025-05-05 11:46:25 +0900

    client.tsと.env.localの追加

---
+++ src/libs/client.ts
@@ -0,0 +1,16 @@
+import { createClient } from 'microcms-js-sdk';
+
+// 環境変数が設定されていない場合はエラーを投げる
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error('MICROCMS_SERVICE_DOMAIN is required');
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error('MICROCMS_API_KEY is required');
+}
+
+// microCMS clientの初期化
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
